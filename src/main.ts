14ec61c30970aac638c258cab8f7421cf951915b
lobzhana <globzhanidze>
2021-02-14 22:04:28 +0400

    moved to folder

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,5 @@
 import assert from './tools/assert';
 
-const maximumUnits = (boxTypes: number[][], truckSize: number): number => {
-  boxTypes = boxTypes.sort((a, b) => b[1] - a[1]);
-
-  let index = 0;
-  let boxesOnTruck = 0;
-  let itemsOnTruck = 0;
-  while (boxesOnTruck < truckSize && index < boxTypes.length) {
-    boxesOnTruck++;
-    boxTypes[index][0]--;
-    itemsOnTruck += boxTypes[index][1];
-
-    if (!boxTypes[index][0]) index++;
-  }
-
-  return itemsOnTruck;
-};
-
-const maximumUnits2 = (boxTypes: number[][], truckSize: number): number => {
-  boxTypes = boxTypes.sort((a, b) => b[1] - a[1]);
-
-  let itemsOnTruck = 0;
-  let item = boxTypes.shift();
-  while (truckSize && item) {
-    truckSize--;
-    item[0]--;
-    itemsOnTruck += item[1];
-
-    if (item[0] === 0) {
-      item = boxTypes.shift();
-    }
-  }
-
-  return itemsOnTruck;
-};
 
 async function main() {
   const maxUnits = maximumUnits2(
